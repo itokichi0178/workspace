e1f46ffe13e2ef4130edba647a822b0fa1991ac1
itokichi0178 <cobaltblueBH@gmail.com>
2014-06-22 15:37:39 +0900

    ・+ボタン追加 ※+ボタン押下時エラーあり

--- KONY/src/special_interest_group/ateam/kony/ListActivity.java
+++ KONY/src/special_interest_group/ateam/kony/ListActivity.java
@@ -8,10 +8,18 @@ import special_interest_group.ateam.kony.db.AlarmSettingsDao;
 import special_interest_group.ateam.kony.db.AlarmSettingsEntity;
 import special_interest_group.ateam.kony.db.AlarmSettingsHelper;
 import android.app.Activity;
+import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 
 /**
  * アラーム一覧
@@ -20,6 +28,12 @@ public class ListActivity extends Activity
 {
     private ArrayList<HashMap<String, String>> data;
     private HashMap<String, String> map;
+    private Button btnAdd;
+    static final String TAG = "SampleSettings";
+    private AlarmListAdapter listAdapter;
+    
+    static List<AlarmSettingsEntity> alarmSettingsEntityList = new ArrayList<AlarmSettingsEntity>();
+
 
     /**
      * DBアクセスクラス
@@ -93,6 +107,44 @@ public class ListActivity extends Activity
 //                new int[]{R.id.HogeTextId, R.id.PiyoTextId}
 //                );
 //                mListView.setAdapter(adapter);
+        
+        // +ボタン押下時
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+        
+        btnAdd.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                // テキストフィールドの内容をSound列に設定し、INSERT
+                dao.insert(
+                           0
+                         , 5
+                         , 10
+                         , 0
+                         , "月曜日"
+                         , "0"
+                         , 0);
+                loadList();
+            }
+        });
+    }
+    
+    // リスト読み込み
+    protected void loadList()
+    {
+        // リストをクリア
+        listAlarm.clear();
+        
+        // データ全件を取得
+        listAlarm = dao.findAll();
+        for (AlarmSettingsEntity entity : listAlarm)
+        {
+            Log.v(TAG, entity.toString());
+        }
+
+        // リストビューのアダプターにデータリストの変更を通知
+        listAdapter.notifyDataSetChanged();   
     }
 
     @Override
@@ -106,4 +158,51 @@ public class ListActivity extends Activity
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
+    
+    public class AlarmListAdapter extends BaseAdapter
+    {
+        @Override
+        public int getCount()
+        {
+            return alarmSettingsEntityList.size();
+        }
+
+        @Override
+        public Object getItem(int position)
+        {
+            return alarmSettingsEntityList.get(position);
+        }
+
+        @Override
+        public long getItemId(int position)
+        {
+            return position;
+        }
+
+        /**
+         * ListViewの指定行の行イメージを取得
+         */
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent)
+        {
+            TextView memoTextView;
+            View v = convertView;
+            if (v == null)
+            {
+                // 定義したListView の行レイアウト(listrow1.xml) を取得する
+                LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                v = inflater.inflate(R.layout.alarm_row, null);
+            }
+            // データリストから指定位置の AlarmSettingsEntity データを取得
+            AlarmSettingsEntity entity = (AlarmSettingsEntity) getItem(position);
+            if (entity != null)
+            {
+                // 取得した AlarmSettingsEntityデータを行レイアウト定義を使って行イメージを作成
+                //memoTextView = (TextView) v.findViewById(R.id.textview_alarmsettingsInfo);
+                //memoTextView.setText("rowId列=[" + entity.getRowId() + "] Sound列=[" + entity.getSound() + "]");
+            }
+            return v;
+        }
+    }
+
 }
